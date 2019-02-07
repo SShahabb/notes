88c8698243be353a881c3f00c84199ed35766b21
SShahabb <ssalehi@kent.edu>
2019-02-07 02:46:39 +0000

    switched to json and added simple ajax

--- notes.py
+++ notes.py
@@ -1,23 +1,24 @@
 # notes
 import os
-import pickle
+import json
+#import pickle
 
 the_notes = []
 
 #get list of notes
 def get_notes():
     global the_notes
-    if os.path.exists("notes.pkl"):
-        with open("notes.pkl","rb") as f:
-            the_notes = pickle.load(f)
+    if os.path.exists("notes.json"):
+        with open("notes.json","r") as f:
+            the_notes = json.load(f)
     return the_notes
 
 #add note to the list
 def add_note(note):
     global the_notes
-    if os.path.exists("notes.pkl"):
-        with open("notes.pkl","rb") as f:
-            the_notes = pickle.load(f)
+    if os.path.exists("notes.json"):
+        with open("notes.json","r") as f:
+            the_notes = json.load(f)
     the_notes.append(note)
-    with open("notes.pkl","wb") as f:
-        pickle.dump(the_notes, f)
+    with open("notes.json","w") as f:
+        json.dump(the_notes, f)
