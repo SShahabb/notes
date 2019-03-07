1ad60ee8d5d3c6b36703644277115a9d754217ed
Greg <greg@Gregs-MacBook-Pro.local>
2019-03-06 21:13:08 -0500

    Updates for step arguments

--- testing/behave_tests/features/amazon.feature
+++ testing/behave_tests/features/amazon.feature
@@ -11,3 +11,15 @@ Feature: Amazon search capability
     When    we search for "toaster"
     Then    we will get at least 20 results
      and    75% of the results will contain "toaster"
+
+  Scenario: Search for a "mixer"
+    Given   We are at www.amazon.com
+    When    we search for "mixer"
+    Then    we will get at least 20 results
+     and    75% of the results will contain "mixer"
+
+  Scenario: Search for a "wok"
+    Given   We are at www.amazon.com
+    When    we search for "wok"
+    Then    we will get at least 20 results
+     and    75% of the results will contain "wok"
--- testing/behave_tests/features/steps/amazon.py
+++ testing/behave_tests/features/steps/amazon.py
@@ -8,11 +8,11 @@ def step_impl(context):
     context.browser.get("https://www.amazon.com")
     assert "amazon" in context.browser.title.lower()
 
-@when(u'we search for "blender"')
-def step_impl(context):
+@when(u'we search for "{item}"')
+def step_impl(context, item):
     search_box = context.browser.find_element_by_id("twotabsearchtextbox")
     search_box.clear()
-    search_box.send_keys("blender")
+    search_box.send_keys(item)
     search_box.send_keys(Keys.RETURN)
 
 @then(u'we will get at least 20 results')
@@ -20,25 +20,10 @@ def step_impl(context):
     context.result_items = context.browser.find_elements_by_class_name("s-result-item")
     assert len(context.result_items) >= 20
 
-@then(u'75% of the results will contain "blender"')
-def step_impl(context):
+@then(u'75% of the results will contain "{item}"')
+def step_impl(context, item):
     n = 0
-    for item in context.result_items:
-        if "blender" in item.text.lower():
-            n += 1
-    assert n * 4 >= len(context.result_items) * 3
-
-@when(u'we search for "toaster"')
-def step_impl(context):
-    search_box = context.browser.find_element_by_id("twotabsearchtextbox")
-    search_box.clear()
-    search_box.send_keys("toaster")
-    search_box.send_keys(Keys.RETURN)
-
-@then(u'75% of the results will contain "toaster"')
-def step_impl(context):
-    n = 0
-    for item in context.result_items:
-        if "toaster" in item.text.lower():
+    for result_item in context.result_items:
+        if item in result_item.text.lower():
             n += 1
     assert n * 4 >= len(context.result_items) * 3
